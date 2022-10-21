ec6705561d72a30803d20d0bc944ebdf4f4d8848
AlbertoMartinez <albertomtzu@gmail.com>
2022-10-21 17:00:44 -0500

    Initial Commit

---
+++ GameStream/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GameStream
+//
+//  Created by ALBERTO on 21/10/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GameStream/GameStreamApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameStreamApp.swift
+//  GameStream
+//
+//  Created by ALBERTO on 21/10/22.
+//
+
+import SwiftUI
+
+@main
+struct GameStreamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
